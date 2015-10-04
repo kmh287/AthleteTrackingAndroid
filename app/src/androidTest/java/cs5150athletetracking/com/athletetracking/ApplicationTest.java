fc139c217222e27a78dc031b9b529add6702aaea
Kevin Howald <kmh287@cornell.edu>
2015-10-04 16:14:08 -0400

    Initial commit!

---
+++ app/src/androidTest/java/cs5150athletetracking/com/athletetracking/ApplicationTest.java
@@ -0,0 +1,14 @@
+package cs5150athletetracking.com.athletetracking;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/androidTest/java/cs5150athletetracking/com/athletetracking/AthleteTrackerTestCase.java
@@ -0,0 +1,26 @@
+package cs5150athletetracking.com.athletetracking;
+
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+public class AthleteTrackerTestCase extends ApplicationTestCase<Application> {
+
+    // All of our test cases should extend this class.
+    // Add any methods / values to this class that you think will be useful in
+    // more than one test.
+
+    public AthleteTrackerTestCase(){
+        super(Application.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
