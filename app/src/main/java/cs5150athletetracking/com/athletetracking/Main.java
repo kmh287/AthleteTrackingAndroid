954bc4aa9b91e55d96713e9381097f6f2bd61517
Kevin Howald <kmh287@cornell.edu>
2015-10-13 18:15:50 -0400

    Revert "Uploaded Uploader".
    This reverts commit 060f7db5327136a82e9458b515f83008c5793d70.

--- app/src/main/java/cs5150athletetracking/com/athletetracking/Main.java
+++
@@ -1,9 +0,0 @@
-// Main Class
-
-public class Main {
-	public static void main(String[] args) {
-		Uploader uploader = new Uploader("Emilio Torres", 21, 0, 25.5, 26.6, 0);
-		Thread thread = new Thread(uploader);
-		thread.start();
-	}
-}
