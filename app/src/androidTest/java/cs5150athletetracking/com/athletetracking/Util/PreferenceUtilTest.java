508ef8ea529b4b85b98aff6b563867f5482526ec
Kevin Howald <kmh287@cornell.edu>
2015-11-04 12:45:19 -0500

    some layout for tests

---
+++ app/src/androidTest/java/cs5150athletetracking/com/athletetracking/Util/PreferenceUtilTest.java
@@ -0,0 +1,12 @@
+package cs5150athletetracking.com.athletetracking.Util;
+
+
+import cs5150athletetracking.com.athletetracking.AthleteTrackerTestCase;
+
+public class PreferenceUtilTest extends AthleteTrackerTestCase {
+
+    public void testWriteAndRead(){
+
+    }
+
+}
